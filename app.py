37dfb52be90dedb33a55e44e5f7f8b07f40c50b8
gary950290 <gary950290@gmail.com>
2025-12-31 17:11:10 +0800

    Update title and add analysis mode selection

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import streamlit as st
+from modules.price_fundamental import get_price, get_fundamentals
 
-st.title("📈 我的第一個美股分析工具")
+st.title("📊 美股分析儀表板")
 
 symbol = st.text_input("輸入股票代碼", "NVDA")
+mode = st.selectbox("選擇分析模式", ["股價", "估值"])
 
-st.write("你輸入的是：", symbol)
+if mode == "股價":
+    data = get_price(symbol)
+    st.metric("股價", data["price"], f'{data["change"]:.2f}%')
+
+if mode == "估值":
+    st.table(get_fundamentals(symbol))
